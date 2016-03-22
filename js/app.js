bfef53b8b526d153896470fea88402571447b193
Willie Tong <tongsinyin@gmail.com>
2016-03-22 14:01:31 +0800

    add number of tasks to tabs

--- js/app.js
+++ js/app.js
@@ -3,15 +3,13 @@ angular
   .config(MainRouter);
 
 function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "/",
+    templateUrl: "home.html",
+  }).state('archive', {
+    url: "/archive",
+    templateUrl: "archive.html",
+  });
+
   $urlRouterProvider.otherwise('/');
-  
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "home.html",
-    })
-    .state('archive', {
-      url: "/archive",
-      templateUrl: "archive.html",
-    });
 }
